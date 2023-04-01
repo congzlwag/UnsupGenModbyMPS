fixed kl divergence and add bond dim restriction

BStest/BS_main.py
@@ -59,19 +59,19 @@ def statistic(current):
 
 
 if __name__ == '__main__':
+	"""The binary number form of BS is stored in BSind.npy, with the identical 
+	order with BSdata.npy"""
 	dataset = np.load('BStest/BSdata.npy').reshape(-1, 16)
-	"""The binary number form of BS is stored in BSind.npy, with the identical order with BSdata.npy"""
-	m = MPS_c(16)
+	m = MPS_c(16, max_bond_dim=4)
 	m.left_cano()
 	m.designate_data(dataset)
 	m.init_cumulants()
-
 	m.cutoff = 5e-5
 	m.descent_step_length = 0.05
 	m.descent_steps = 10
 	m.train(2)
 
-	m.saveMPS('BS-',True)
+	m.saveMPS(f'BS-{m.maxibond}-',True)
 	sam_zip = remember_zipper(m, 1000)
 	# os.chdir('BS-MPS')
 	np.save('sam_zip.npy', sam_zip)
@@ -79,3 +79,6 @@ if __name__ == '__main__':
 	sam_met = remember(m, 5000, 1000)
 	np.save('sam_met.npy', sam_met)
 	np.save('memo_met.npy', statistic(sam_met))
+
+	
+	# TODO adding custom constraint on the bond dimension here

MPScumulant.py
@@ -12,7 +12,7 @@ import os
 import sys
 
 class MPS_c:
-	def __init__(self, space_size):
+	def __init__(self, space_size, max_bond_dim=300):
 		"""
 		MPS class, with cumulant technique, efficient in DMRG-2
 		Attributes:
@@ -48,7 +48,7 @@ class MPS_c:
 		# bond[i] connects i & i+1
 		init_bondim = 2
 		self.minibond = 2
-		self.maxibond = 300
+		self.maxibond = max_bond_dim
 		self.bond_dimension = init_bondim * \
 			ones((space_size,), dtype=np.int16) 
 		self.bond_dimension[-1] = 1

metrics.py
@@ -39,11 +39,30 @@ def kl_divergence_estimate(probs):
             in our dataset
 
     Returns:
-        (qml.np.tensor) : Floating point value of the KL-divergence
+        (qml.np.tensor) : Floating point value of an estimate KL-divergence
 
     # TODO can this be used as a cost function to train the QCBM
     """
     # Uniform empirical probability of the bitstrings in the dataset
     p_data = pnp.tensor(1./len(probs), requires_grad=False)
+    log_p_over_q = pnp.log(probs) - pnp.log(p_data)
+    return pnp.dot(probs, log_p_over_q)
+
+
+def kl_divergence(probs, data):
+    """
+    compute the kl divergence over the entire space of the bitstrings
+
+    Args:
+        probs (qml.np.tensor) : Output of the qnode - probabilities for each
+            basis state
+
+    Returns:
+        (qml.np.tensor) : Floating point value of the KL-divergence
+    """
+    # p_data with epsilon added to avoid log(0) error
+    p_data = pnp.zeros(len(probs), requires_grad=False) + 1e-16
+    idxs = np.dot(data, 2**np.arange(data.shape[1])[::-1])
+    p_data[idxs] = 1./data.shape[0]
     log_p_over_q = pnp.log(probs) - pnp.log(p_data)
     return pnp.dot(probs, log_p_over_q)